99da478b5a2211149f5b2b85d1e03553f6080fe7
trungnpm <npmtrung@gmai.com>
2021-09-20 20:38:03 +0700

    Commit Excercise_13_BMI

--- src/main/java/Excercises/Ex13/CheckException.java
+++ src/main/java/Excercises/Ex13/CheckException.java
@@ -4,4 +4,14 @@ public class CheckException extends Exception{
     public CheckException(String message) {
         super(message);
     }
+    public static boolean isDouble(String x) { //check số kiểu double
+        if (x == null)
+            return false;
+        try {
+            double a = Double.parseDouble(x);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/Excercises/Ex13/Excercise_13.java
+++ src/main/java/Excercises/Ex13/Excercise_13.java
@@ -2,17 +2,30 @@ package Excercises.Ex13;
 
 import java.util.Scanner;
 
+import static Excercises.Ex13.CheckException.isDouble;
+
 public class Excercise_13 {
     public static void main(String[] args) throws CheckException {
         Scanner scanner = new Scanner(System.in);
         try {
+            double tempCanNang, tempChieuCao;
             System.out.print("Nhập cân nặng(kg): ");
-            double canNang = scanner.nextDouble();
-            if (canNang <= 0) throw new CheckException("Cân nặng không hợp lệ");
+            String canNang = scanner.nextLine();
+            if (isDouble(canNang)) {
+                tempCanNang = Double.parseDouble(canNang);
+            } else {
+                throw new CheckException("Cân nặng không đúng định dạng");
+            }
+            if (tempCanNang <= 0) throw new CheckException("Cân nặng không hợp lệ");
             System.out.print("Nhập chiều cao(m): ");
-            double chieuCao = scanner.nextDouble();
-            if (chieuCao <= 0) throw new CheckException("Chiều cao không hợp lệ");
-            BMI.calculateBMI(canNang, chieuCao);
+            String chieuCao = scanner.nextLine();
+            if (isDouble(canNang)) {
+                tempChieuCao = Double.parseDouble(chieuCao);
+            } else {
+                throw new CheckException("Chiều cao không đúng định dạng");
+            }
+            if (tempChieuCao <= 0) throw new CheckException("Chiều cao không hợp lệ");
+            BMI.calculateBMI(tempCanNang, tempCanNang);
         } catch (Exception e) {
             e.printStackTrace();
         }
