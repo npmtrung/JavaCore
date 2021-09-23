70b30870cffbb2621368f93a7481946ecaa043d6
trungnpm <trung.nguyen18@onemount.com>
2021-09-23 09:46:55 +0700

    Commit Ex_14_Regex

---
+++ src/main/java/Excercises/Ex14/RegexEx1.java
@@ -0,0 +1,16 @@
+package Excercises.Ex14;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexEx1 {
+    public static void main(String[] args) {
+        String text1 = "Hello java regex 2-12-2018, hello world 12/12/2018";
+        Pattern pattern = Pattern.compile("\\d{1,2}[-|/]\\d{1,2}[-|/]\\d{4}");
+        Matcher matcher = pattern.matcher(text1);
+
+        System.out.println("Ngày tháng trong chuỗi text1: " + text1);
+        while (matcher.find()) {
+            System.out.println(text1.substring(matcher.start(), matcher.end()));
+        }
+    }
+}
