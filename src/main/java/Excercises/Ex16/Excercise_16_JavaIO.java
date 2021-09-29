458f1f5865e8b2733c331b71d54dd9b0c9dd31c2
trungnpm <npmtrung@gmai.com>
2021-09-29 19:21:14 +0700

    Commit Excercise_16_JavaIO

---
+++ src/main/java/Excercises/Ex16/Excercise_16_JavaIO.java
@@ -0,0 +1,33 @@
+package Excercises.Ex16;
+
+import java.io.File;
+
+public class Excercise_16_JavaIO {
+    public static void main(String[] args) {
+        String workdirPath = System.getProperty("user.dir");
+        System.out.printf("Workdir: %s\n", workdirPath);
+
+        File workdir = new File(workdirPath);
+        String[] list = workdir.list();
+        for (String str : list) {
+            System.out.println(str);
+        }
+
+        File[] listFiles = workdir.listFiles();
+        for (File file : listFiles) {
+            System.out.print(file.getPath());
+            System.out.print(" > ");
+            System.out.println(file.isDirectory() ? "Folder" : "File");
+        }
+    }
+
+    private static void listFilesinDirectory(File dir) {
+        File[] listFiles = dir.listFiles();
+        for (File file : listFiles) {
+            if (file.isFile()) {
+                if (file.getName().compareTo(".DS_Store") != 0)
+                    System.out.println(file.getName());
+            } else listFilesinDirectory(file);
+        }
+    }
+}
