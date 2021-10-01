ab7949036e2e3233f9820997252e5e0bce41db47
trungnpm <npmtrung@gmai.com>
2021-10-01 20:06:58 +0700

    Commit Excercise_17_JavaFileIO

---
+++ src/main/java/Excercises/Ex17/JavaTextFileIO.java
@@ -0,0 +1,41 @@
+package Excercises.Ex17;
+
+import java.io.*;
+
+public class JavaTextFileIO {
+    public static void main(String[] args) throws IOException {
+        PrintWriter pw = new PrintWriter("C.txt");
+
+        FileOutputStream fosA = new FileOutputStream("A.txt");
+        PrintWriter pwA = new PrintWriter(fosA);
+        pwA.println("Example text in A file");
+        pwA.close();
+        BufferedReader br = new BufferedReader(new FileReader("A.txt"));
+
+        String line = br.readLine();
+        while (line != null) {
+            pw.println(line);
+            line = br.readLine();
+        }
+        FileOutputStream fosB = new FileOutputStream("B.txt");
+        PrintWriter pwB = new PrintWriter(fosB);
+        pwB.println("Example text in B file");
+        pwB.close();
+        br = new BufferedReader(new FileReader("B.txt"));
+
+        line = br.readLine();
+        while (line != null) {
+            pw.println(line);
+            line = br.readLine();
+        }
+        pw.flush();
+        br.close();
+        pw.close();
+        try (BufferedReader brC = new BufferedReader(new FileReader("C.txt"))) {
+            String lineC = null;
+            while ((lineC = brC.readLine()) != null) {
+                System.out.println(lineC);
+            }
+        }
+    }
+}
